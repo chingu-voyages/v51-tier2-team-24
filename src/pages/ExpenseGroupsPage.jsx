e3fbf20fc583357a0275d06d71320d64003386ac
Ruslan <ruslan.berendeev@gmail.com>
2024-09-30 22:57:03 +0100

    Refactor Expense Group Page in order to implement add /edit interfaces

--- src/pages/ExpenseGroupsPage.jsx
+++ src/pages/ExpenseGroupsPage.jsx
@@ -1,49 +1,74 @@
-import GridCard from "@/components/GridCard";
-import { PageGrid } from "@/components/PageGrid";
-import { PageHeader } from "@/components/PageHeader";
-import { BodyText } from "@/components/Typography";
+import GridCard from "@/components/GridCard"
+import { PageGrid } from "@/components/PageGrid"
+import { PageHeader } from "@/components/PageHeader"
+import { BodyText } from "@/components/Typography"
+import { Button } from "@/components/ui/button"
+import { formatCurrency } from "@/lib/utils"
+import { Link } from "react-router-dom"
+
+const MOCK_GROUP_INFO = [
+  { name: "Bali Trip", avatar: "#", id: "random_id_1", budget: 500, expense: 300, members: 5 },
+  { name: "Hawaii Trip", avatar: "#", id: "random_id_2", budget: 500, expense: 300, members: 5 },
+  {
+    name: "Mike's Birthday",
+    avatar: "#",
+    id: "random_id_3",
+    budget: 500,
+    expense: 300,
+    members: 5,
+  },
+  {
+    name: "John's Birthday",
+    avatar: "#",
+    id: "random_id_4",
+    budget: 500,
+    expense: 300,
+    members: 5,
+  },
+  { name: "Escape Room", avatar: "#", id: "random_id_5", budget: 500, expense: 300, members: 5 },
+]
 
 export function ExpenseGroupsPage() {
-  const MOCK_GROUP_INFO = [
-    { name: "Bali Trip", avatar: "#", id: "random_id_1", budget: 500, expense: 300, members: 5 },
-    { name: "Hawaii Trip", avatar: "#", id: "random_id_2", budget: 500, expense: 300, members: 5 },
-    { name: "Mike's Birthday", avatar: "#", id: "random_id_3", budget: 500, expense: 300, members: 5 },
-    { name: "John's Birthday", avatar: "#", id: "random_id_4", budget: 500, expense: 300, members: 5 },
-    { name: "Escape Room", avatar: "#", id: "random_id_5", budget: 500, expense: 300, members: 5 },
-  ]
-
-  const groupList = MOCK_GROUP_INFO.map(group => {
+  const groupList = MOCK_GROUP_INFO.map((group) => {
     return (
       <GridCard
         key={group.id}
         name={group.name}
         avatarUrl="#"
-        actions="Edit"
         content={
           <>
-            <BodyText tag="p" variant="normal" className="mb-1">Alloted Budget: ${group.budget}</BodyText>
-            <BodyText tag="p" variant="normal" className="mb-1">Expense Amount: ${group.expense}</BodyText>
-            <BodyText tag="p" variant="normal" className="mb-1">Participants: {group.members}</BodyText>
+            <BodyText
+              tag="dl"
+              className="grid grid-cols-[auto_1fr] gap-x-4 gap-y-2 [&>dt]:font-medium mb-2"
+            >
+              <dt>Alloted Budget: </dt>
+              <dd>{formatCurrency(group.budget)}</dd>
+              <dt>Expense Amount: </dt>
+              <dd>{formatCurrency(group.expense)}</dd>
+              <dt>Participants: </dt>
+              <dd>{group.members}</dd>
+            </BodyText>
           </>
         }
-        footer="See More"
+        footer={
+          <div className="w-full flex justify-end">
+            <Button asChild variant="link" to={`/app/groups/${group.id}`}>
+              <Link>See more</Link>
+            </Button>
+          </div>
+        }
       />
     )
   })
   return (
     <>
-      <PageGrid 
-        children={
-          <PageHeader 
-            children="Groups"
-            subheading="Detailed overview of all your groups"
-            headerAction="New Group"
-          />
-        } 
-      />
-      <PageGrid>
-      {groupList} 
-      </PageGrid>
+      <PageHeader
+        subheading="Detailed overview of all your groups"
+        headerAction={<Button>New Group</Button>}
+      >
+        Groups
+      </PageHeader>
+      <PageGrid>{groupList}</PageGrid>
     </>
   )
 }
