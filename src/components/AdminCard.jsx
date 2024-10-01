3ee57f6281afdeca7255995678d7fa83ea1cf59e
Ruslan <ruslan.berendeev@gmail.com>
2024-10-01 11:31:56 +0100

    Add edit interface for admin details

--- src/components/AdminCard.jsx
+++ src/components/AdminCard.jsx
@@ -2,22 +2,35 @@ import { Pencil } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "./ui/button"
 import { Card } from "./ui/card"
-import { cn, getInitials } from "@/lib/utils"
+import { cn, getNameDetails } from "@/lib/utils"
 import PropTypes from "prop-types"
-
-const MOCK_ADMIN_NAME = "Alex Norman"
+import { ResponsiveDialog } from "./ResponsiveDialog"
+import AdminForm from "./forms/AdminForm"
+import { ADMIN_MOCK_DATA } from "@/lib/mock-data"
 
 export function AdminCard({ className }) {
+  const { fullName, initials } = getNameDetails({
+    firstName: ADMIN_MOCK_DATA.firstName,
+    lastName: ADMIN_MOCK_DATA.lastName,
+  })
   return (
     <Card className={cn("p-2 border flex items-center gap-4", className)}>
       <Avatar>
         <AvatarImage src="#" />
-        <AvatarFallback>{getInitials(MOCK_ADMIN_NAME)}</AvatarFallback>
+        <AvatarFallback>{initials}</AvatarFallback>
       </Avatar>
-      <p className="font-bold mr-auto">{MOCK_ADMIN_NAME}</p>
-      <Button size="icon" variant="ghost" className="rounded-full shrink-0">
-        <Pencil className="size-4" />
-      </Button>
+      <p className="font-bold mr-auto">{fullName}</p>
+      <ResponsiveDialog
+        dialogTitle="Edit Admin Details"
+        dialogDescription="Make changes to your admin profile here. Click save when you're done."
+        trigger={
+          <Button size="icon" variant="ghost" className="rounded-full shrink-0">
+            <Pencil className="size-4" />
+          </Button>
+        }
+      >
+        <AdminForm defaultValues={ADMIN_MOCK_DATA} />
+      </ResponsiveDialog>
     </Card>
   )
 }
